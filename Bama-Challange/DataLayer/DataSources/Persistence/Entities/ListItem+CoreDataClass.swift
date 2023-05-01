86925f5cd0ab82163bc003a0a28c283475120090
Kiarash Vosough <vosough.k@gmail.com>
2023-05-01 12:06:13 +0330

    added coredata classes

---
+++ Bama-Challange/DataLayer/DataSources/Persistence/Entities/ListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ListItem+CoreDataClass.swift
+//  Bama-Challange
+//
+//  Created by Kiarash Vosough on 4/30/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ListItem)
+public class ListItem: NSManagedObject {
+
+}
---
+++ Bama-Challange/DataLayer/DataSources/Persistence/Entities/User+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  User+CoreDataClass.swift
+//  Bama-Challange
+//
+//  Created by Kiarash Vosough on 4/30/23.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(User)
+public class User: NSManagedObject {
+
+}
