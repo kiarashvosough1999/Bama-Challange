3e80405e31ab87197ccef1eed194dd9485a384da
Kiarash Vosough <vosough.k@gmail.com>
2023-05-04 21:43:29 +0330

    added string mock

---
+++ Bama-ChallangeTests/MockPrimitives/String++Mocks.swift
@@ -0,0 +1,12 @@
+//
+//  String++Mocks.swift
+//  Bama-ChallangeTests
+//
+//  Created by Kiarash Vosough on 5/4/23.
+//
+
+import Foundation
+
+extension String {
+    internal static let shortValue: String = "test"
+}
